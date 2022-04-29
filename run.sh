a67cfd37145dff20367b44c6aa6973d479ae7f5a
tlylt <liu_yongliang@hotmail.com>
2022-04-30 00:48:49 +0800

    Fix script

--- run.sh
+++ run.sh
@@ -8,11 +8,17 @@
 ### ./get-reposense.py --commit abc123         # Gets a specific commit
 ### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
 
-./get-reposense.py --release
+version=$1
+
+if [[ -n "$version" ]]; then
+    ./get-reposense.py --$version  
+else
+    ./get-reposense.py --release
+fi
 
 # Executes RepoSense
 # Do not change the default output folder name (reposense-report)
 ## Examples of other valid options; For more, please view the user guide
 ### java -jar RepoSense.jar --repos https://github.com/reposense/RepoSense.git
 
-java -jar RepoSense.jar --config ./configs
+# java -jar RepoSense.jar --config ./configs
